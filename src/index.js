5ac20e7ece8cf7c538baa90dd484ff2f47ab0d7d
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-08-17 19:02:43 +0300

    done+-

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import 'modern-normalize/modern-normalize.css';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/lib/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/goit-react-hw-07-phonebook">
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </BrowserRouter>
+  // <BrowserRouter basename="/goit-react-hw-07-phonebook">
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+  // </BrowserRouter>
   // </React.StrictMode>
 );
